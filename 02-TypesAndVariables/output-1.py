3aeaeec342f1951970e691ecf9164759c8182732
Jakub Pniewski <jakub.pniewski@icloud.com>
2024-10-08 14:11:26 +0200

    output formatting done

---
+++ 02-TypesAndVariables/output-1.py
@@ -0,0 +1,10 @@
+###
+# Printing student's personal data
+#
+name = "Adam"
+years_old = 22
+height = 180
+
+print(f"My name is {name}.")
+print(f"I am {years_old} years old, and my height is {height} cm.
+In 6 years I will be {years_old + 6} years old")
