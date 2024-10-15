c34f93aec9b7831a13e3254e2eaf8edcc5d6efb2
Jakub Pniewski <jakub.pniewski@icloud.com>
2024-10-15 12:03:23 +0200

    last chapter in 3rd section in progress

--- 03-ControlStructures/atm.py
+++ 03-ControlStructures/atm.py
@@ -5,14 +5,20 @@ balance = 1000  # Initial balance
 pin = '1111' # initial 4-digit PIN code
 
 while True:
-    print()
+    entered_pin = input("Enter Password: ")
+
+    if entered_pin != pin:
+        print('You entered wrong pin')
+        continue
+
     print("ATM Menu:")
     print("1. Check balance")
     print("2. Deposit")
     print("3. Withdraw")
     print("4. Exit")
+    print("5. change PIN")
 
-    choice = input("Choose an option (1-4): ")
+    choice = input("Choose an option (1-5): ")
     print()
 
     if choice == '1':
@@ -31,5 +37,20 @@ while True:
     elif choice == '4':
         print("Exiting... Thank you for using the ATM!")
         break  # Exit the loop
+    elif choice == '5':
+        is_pin_changed = False
+        while not is_pin_changed:
+            new_pin = input("Enter new PIN: ")
+            is_valid = False
+            if len(new_pin) == 4:
+                for char in new_pin:
+                    if  char.isdigit():
+                        is_valid = True
+            if not is_valid:
+                print("PIN should contain only 4 digits")
+            if is_valid:
+                pin = new_pin
+                is_pin_changed = True
+            
     else:
         print("Invalid option. Please try again.")
